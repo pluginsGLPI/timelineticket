63649bf5341090b1b954bd54e22e27368e649fad
David Durieux <ddurieux@users.noreply.github.com>
2013-10-04 09:59:58 +0000

    Update unit tests for glpi 0.84

--- phpunit/GLPIInstall/AllTests.php
+++ phpunit/GLPIInstall/AllTests.php
@@ -47,20 +47,27 @@ class GLPIInstall extends PHPUnit_Framework_TestCase {
 
    public function testInstall() {
       global $DB;
-      
+
+      $query = "SHOW FULL TABLES WHERE TABLE_TYPE LIKE 'VIEW'";
+      $result = $DB->query($query);
+      while ($data=$DB->fetch_array($result)) {
+         $DB->query("DROP VIEW ".$data[0]);
+      }
+
       $query = "SHOW TABLES";
       $result = $DB->query($query);
       while ($data=$DB->fetch_array($result)) {
          $DB->query("DROP TABLE ".$data[0]);
       }
-      
+
       include_once (GLPI_ROOT . "/inc/dbmysql.class.php");
       include_once (GLPI_CONFIG_DIR . "/config_db.php");
-      
-      // Install a fresh 0.83 DB
+
+      // Install a fresh 0.84 DB
       $DB  = new DB();
-      $res = $DB->runFile(GLPI_ROOT ."/install/mysql/glpi-0.83.1-empty.sql");
+      $res = $DB->runFile(GLPI_ROOT ."/install/mysql/glpi-0.84.1-empty.sql");
       $this->assertTrue($res, "Fail: SQL Error during install");
+      $DB->query("UPDATE `glpi_configs` SET `version`='0.84.3' WHERE `id`='1'");
 
       // update default language
       $query = "UPDATE `glpi_configs`
@@ -69,7 +76,7 @@ class GLPIInstall extends PHPUnit_Framework_TestCase {
       $query = "UPDATE `glpi_users`
                 SET `language` = 'fr_FR'";
       $this->assertTrue($DB->query($query), "Fail: can't set users language");
-      
+
       $GLPIlog = new GLPIlogs();
       $GLPIlog->testSQLlogs();
       $GLPIlog->testPHPlogs();
@@ -87,4 +94,5 @@ class GLPIInstall_AllTests  {
    }
 }
 
+?>
 ?>
