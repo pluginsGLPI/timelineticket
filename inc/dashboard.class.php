b1786a7469767ebdf9035e7263f3f9c0200b386f
ales-infotel <alban.lesellier@infotel.com>
2021-03-29 16:08:09 +0200

    min fix comment

--- inc/dashboard.class.php
+++ inc/dashboard.class.php
@@ -2,28 +2,28 @@
 /*
  * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
  -------------------------------------------------------------------------
- Insightvm plugin for GLPI
- Copyright (C) 2018 by the Insightvm Development Team.
+ timelineticket plugin for GLPI
+ Copyright (C) 2018 by the timelineticket Development Team.
 
- https://github.com/InfotelGLPI/Insightvm
+ https://github.com/pluginsGLPI/timelineticket
  -------------------------------------------------------------------------
 
  LICENSE
 
- This file is part of Insightvm.
+ This file is part of timelineticket.
 
- Insightvm is free software; you can redistribute it and/or modify
+ timelineticket is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- Insightvm is distributed in the hope that it will be useful,
+ timelineticket is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with Insightvm. If not, see <http://www.gnu.org/licenses/>.
+ along with timelineticket. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
  */
 
