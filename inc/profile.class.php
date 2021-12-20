81b344ec161fe74e30ca08369e08f08886a2c52b
tsmr <thetsmr@gmail.com>
2021-12-20 16:13:48 +0100

    Fix css

--- inc/profile.class.php
+++ inc/profile.class.php
@@ -151,7 +151,7 @@ class PluginTimelineticketProfile extends Profile {
           && $closeform) {
          echo "<div class='center'>";
          echo Html::hidden('id', ['value' => $profiles_id]);
-         echo Html::submit(_sx('button', 'Save'), ['name' => 'update']);
+         echo Html::submit(_sx('button', 'Save'), ['name' => 'update', 'class' => 'btn btn-primary']);
          echo "</div>\n";
          Html::closeForm();
       }
