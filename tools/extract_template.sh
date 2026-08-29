70655abfad00d81b1b653a0fcd084b5b1b593e5d
Thetsmr <thetsmr@gmail.com>
2026-08-29 12:56:14 +0200

    Fix extract_template.sh

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -38,7 +38,12 @@
 
 xgettext *.php */*.php */*/*.php --copyright-holder='Timelineticket Development Team' --package-name='GLPI - Timelineticket plugin' --package-version='0.90+1.1' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
 	--keyword=_n:1,2,4t --keyword=__s:1,2t --keyword=__:1,2t --keyword=_e:1,2t --keyword=_x:1c,2,3t \
-	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t
+	--keyword=_ex:1c,2,3t --keyword=_nx:1c,2,3,5t --keyword=_sx:1c,2,3t \
+	`# php-cs-fixer adds a trailing comma to every multiline call, and xgettext counts it as` \
+	`# one extra argument, so the specs above stop matching and strings are silently dropped.` \
+	`# These duplicates accept the same calls with that extra argument. Keep both lists in sync.` \
+	--keyword=_n:1,2,5t --keyword=__s:1,3t --keyword=__:1,3t --keyword=_e:1,3t --keyword=_x:1c,2,4t \
+	--keyword=_ex:1c,2,4t --keyword=_nx:1c,2,3,6t --keyword=_sx:1c,2,4t
 
 
 
